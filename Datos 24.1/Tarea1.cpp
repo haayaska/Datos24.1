fd693f801ff5ec60cfbe4be6301d14427e6d32a8
flawless0011 <franciscopino1236@gmail.com>
2024-04-07 19:15:13 -0400

    aaa

--- Datos 24.1/Tarea1.cpp
+++ Datos 24.1/Tarea1.cpp
@@ -3,33 +3,44 @@
 using namespace std;
 
 struct Pieza {
- char simbolo; // Define qué tipo de pieza es y su caracter
- int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
+    char simbolo; // Define qué tipo de pieza es y su caracter
+    int x, y; // Su posición dentro del tablero [0, 7] x [0, 7]
 };
 
 struct Tablero {
-  int cantidad_piezas;
-  Pieza *piezas_tablero;
+    int cantidad_piezas;
+    Pieza *piezas_tablero;
 };
 
 bool tableroEnJaqueMate(Tablero &tablero);
 
-int main(){
-    char tablerito[64];
+int main() {
     fstream file;
     file.open("tablero.txt", ios::in);
     int npiezas;
-    file>> npiezas ;
+    file >> npiezas;
+    char array[npiezas];
     cout << npiezas << endl;
-    char no;
-    for(int cont = 0; cont<= 63 ;cont++){
-      file >> no;
-      cout << no;
-      if((cont+1)%8==0){
-        cout<<"\n";
-      }
-    }
-    file.close();
-    return 0;
 
+    char no;
+    for (int cont = 0; cont < npiezas; cont++) {
+        file >> no;
+        cout << no;
+
+        if (no != '.') {
+            array[cont] = no;
+        }
+
+        if ((cont + 1) % 8 == 0) {
+            cout << "\n";
+        }
+    }
+
+    file.close();
+
+    for (int i = 0; i < npiezas-1; i++) {
+        cout << array[i] << endl;
+    }
+
+    return 0;
 }
